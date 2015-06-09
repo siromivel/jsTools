e370aaa7246e6e1327bf2afcd393092e648624f2
Levi S. Morris <siromivel@gmail.com>
2015-06-09 12:40:56 -0600

    Bringing consitency to the tabs

--- elementReplace.js
+++ elementReplace.js
@@ -1,26 +1,26 @@
 // This is a function to replace one type of element with another while preserving the content of the original element(s).
 
 function elementReplace(sel) {
-    var selector = sel.split(/(?=\.|#)/);
-    var ssElementArray = [];
+  var selector = sel.split(/(?=\.|#)/);
+  var ssElementArray = [];
 
-    if (selector.length === 1) {
-        if (selector[0].match(/^[^\.#]/)) {
-          ssElementArray = document.getElementsByTagName(selector[0]);
-	    } else if (selector[0].charAt(0) === '.') {
-	      ssElementArray = document.getElementsByClassName(selector[0].substr(1));
-	    } else if (selector[0].charAt(0) === '#') {
-	      ssElementArray = document.getElementById(selector[0].substr(1));
-	    }
-    } else {
-        ssElementArray = document.querySelectorAll(sel);
+  if (selector.length === 1) {
+    if (selector[0].match(/^[^\.#]/)) {
+      ssElementArray = document.getElementsByTagName(selector[0]);
+	} else if (selector[0].charAt(0) === '.') {
+	  ssElementArray = document.getElementsByClassName(selector[0].substr(1));
+	} else if (selector[0].charAt(0) === '#') {
+	  ssElementArray = document.getElementById(selector[0].substr(1));
     }
+  } else {
+    ssElementArray = document.querySelectorAll(sel);
+  }
 
-	var elementsLength = ssElementArray.length;
+  var elementsLength = ssElementArray.length;
 
-	for (var i = 0; i < elementsLength; i++) {
-		ssElementArray[i].outerHTML = ssElementArray[i].outerHTML.replace(/<table|<tbody|<tr|<td+/g, '<div').replace(/<\/table|<\/tbody|<\/tr|<\/td+/g, '</div');
-	}
+  for (var i = 0; i < elementsLength; i++) {
+    ssElementArray[i].outerHTML = ssElementArray[i].outerHTML.replace(/<table|<tbody|<tr|<td+/g, '<div').replace(/<\/table|<\/tbody|<\/tr|<\/td+/g, '</div');
+  }
 
-	return ssElementArray;
+  return ssElementArray;
 }
