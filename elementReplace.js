27aa1ae30417b0d201bede31e4835c1c7764600d
Levi S. Morris <siromivel@gmail.com>
2015-06-12 15:55:14 -0600

    Making an ugly if statement into a beautiful switch.

--- elementReplace.js
+++ elementReplace.js
@@ -4,16 +4,19 @@ function elementReplace(sel) {
   var selector = sel.split(/(?=\.|#)/);
   var ssElementArray = [];
 
-  if (selector.length === 1) {
-    if (selector[0].match(/^[^\.#]/)) {
+  switch (selector) {
+    case selector[0].match(/^[^\.#]/):
       ssElementArray = document.getElementsByTagName(selector[0]);
-	} else if (selector[0].charAt(0) === '.') {
-	  ssElementArray = document.getElementsByClassName(selector[0].substr(1));
-	} else if (selector[0].charAt(0) === '#') {
-	  ssElementArray = document.getElementById(selector[0].substr(1));
-    }
-  } else {
-    ssElementArray = document.querySelectorAll(sel);
+      break;
+    case selector[0].charAt(0) === '.':
+      ssElementArray = document.getElementsByClassName(selector[0].substr(1));
+      break;
+    case selector[0].charAt(0) === '#':
+      ssElementArray = document.getElementById(selector[0].substr(1));
+      break;
+    default:
+      ssElementArray = document.querySelectorAll(sel);
+      break;
   }
 
   var elementsLength = ssElementArray.length;
