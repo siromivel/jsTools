1570caa6bd2ca74cf47ade2b1becd6f5330989b5
Levi S. Morris <siromivel@gmail.com>
2015-06-30 10:54:55 -0600

    Updating the caseChange function.

--- caseChange.js
+++ caseChange.js
@@ -1,24 +1,34 @@
-// This is a function to alter the case of a string to the case specified in the "direction" parameter. For when CSS fails.
-
 var caseChange = function(data, direction) {
   var target = document.querySelectorAll(data);
   var l1 = target.length;
   for (var i = 0; i < l1; i++) {
     var l2 = target[i].innerHTML.length;
-    target[i].innerHTML = target[i].innerHTML.toLowerCase();
+    target[i].innerHTML = target[i].innerHTML.replace(/^\s+/, '').toLowerCase();
+
     switch(direction) {
+      //Entire string to uppercase.
       case "upcase":
         target[i].innerHTML = target[i].innerHTML.toUpperCase();
         break;
+      //Entire string to lowercase.
       case "downcase":
         break;
+      //Capitalize first letter of each word.
       case "first":
-        target[i].innerHTML = target[i].innerHTML.slice(0,1).toUpperCase() + target[i].innerHTML.slice(1, target[i].innerHTML.length).toLowerCase();
+        target[i].innerHTML = target[i].innerHTML.slice(0, 1).toUpperCase() + target[i].innerHTML.slice(1);
+        
+        for (var j = 0; j < l2; j++) {
+          // Check whether or not any given character needs to be capitalized.
+          if (/[\.!?]/.test(target[i].innerHTML[j-1]) && j != target[i].innerHTML.length - 1) {
+            target[i].innerHTML = target[i].innerHTML.slice(0, j + 1) + target[i].innerHTML[j + 1].toUpperCase() + target[i].innerHTML.slice(j + 2).toLowerCase();
+          }
+        }
         break;
+      //Capitalize first letter of each sentence (default behavior).
       case "capitalize":
       case undefined:
         for (var j = 0; j < l2; j++) {
-          // Check whether or not any given character needs to be capitalized
+          // Check whether or not any given character needs to be capitalized.
           if (j === 0 || target[i].innerHTML[j-1] === ' ' && j != target[i].innerHTML.length - 1) {
             target[i].innerHTML = target[i].innerHTML.slice(0, j) + target[i].innerHTML[j].toUpperCase() + target[i].innerHTML.slice(j + 1);
           }
